036e062298fc4c67e65f7580bd14389e2675f732
t-mint <o4510x@gmail.com>
2020-03-31 00:32:06 +0900

    セクション２： 33. 別のページにジャンプする

---
+++ section2/sample22.php
@@ -0,0 +1,6 @@
+<?php
+// 33. 別のページにジャンプする　　
+// header：webページを表示するのか、別のページにジャンプするのかなどの様々な指示をブラウザに行うことができる
+// 今回の「'Location:'」は別のページにジャンプする指示
+header('Location: http://localhost:8888/PHP_no_kyoukasho/bakatter/login.php');
+exit();
